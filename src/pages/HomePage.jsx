7482f27b39bdf925689895e5f16f9c43aee2578b
BardhMustafa <50417505+BardhMustafa@users.noreply.github.com>
2026-03-14 14:40:20 +0100

    feat: replace ContactSection with modal CTA, clean up hash scroll

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
 import { Hero } from "../components/home/Hero";
 import { ServicesSection } from "../components/home/ServicesSection";
 import { AboutSection } from "../components/home/AboutSection";
@@ -8,18 +6,6 @@ import { ContactSection } from "../components/home/ContactSection";
 import { LatestPostsSection } from "../components/home/LatestPostsSection";
 
 export function HomePage() {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.hash) {
-      const id = location.hash.slice(1);
-      const el = document.getElementById(id);
-      if (el) {
-        setTimeout(() => el.scrollIntoView({ behavior: "smooth" }), 100);
-      }
-    }
-  }, [location.hash]);
-
   return (
     <main style={{ position: "relative", zIndex: 1 }}>
       <Hero />
