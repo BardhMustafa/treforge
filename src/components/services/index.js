a1b8b72fa3bf28aa67093cd8534cb9f76cf29bf9
BardhMustafa <50417505+BardhMustafa@users.noreply.github.com>
2026-03-14 16:32:58 +0100

    chore: export StepPanel from services barrel

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -1,3 +1,4 @@
 export { PipelineDiagram } from "./PipelineDiagram";
 export { DashboardMockup } from "./DashboardMockup";
 export { ProductShowcase } from "./ProductShowcase";
+export { StepPanel } from "./StepPanel";
